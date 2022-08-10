e36bfea85cb837e7280b2d0167868795190e5dc2
ukonpower <ore.ukonpower@gmail.com>
2022-08-10 20:42:10 +0900

    wip

---
+++ src/ts/MainScene/World/Sections/Section4/Text/index.ts
@@ -0,0 +1,66 @@
+import * as THREE from 'three';
+import * as ORE from 'ore-three';
+import * as CANNON from 'cannon';
+
+export class Text {
+
+	private commonUniforms: ORE.Uniforms;
+
+	private animator: ORE.Animator;
+
+	public mesh: THREE.Object3D;
+	public body: CANNON.Body;
+	private baseSize: CANNON.Vec3;
+	private boxShape: CANNON.Box;
+
+	constructor( mesh: THREE.Object3D, parentUniforms: ORE.Uniforms ) {
+
+		this.mesh = mesh;
+
+		this.commonUniforms = ORE.UniformsLib.mergeUniforms( parentUniforms, {
+		} );
+
+		/*-------------------------------
+			Animator
+		-------------------------------*/
+
+		this.animator = window.gManager.animator;
+
+		this.animator.add( {
+			name: 'Sec4TextScale' + this.mesh.uuid,
+			initValue: 1,
+		} );
+
+		/*-------------------------------
+			Mesh
+		-------------------------------*/
+
+		let size = new THREE.Box3().setFromObject( this.mesh ).getSize( new THREE.Vector3() );
+
+		this.body = new CANNON.Body( { mass: 1 } );
+		this.baseSize = new CANNON.Vec3( size.x / 2, size.y / 2, size.z / 2 );
+		this.boxShape = new CANNON.Box( this.baseSize.clone() );
+		this.body.addShape( this.boxShape );
+		this.body.position.set( this.mesh.position.x, this.mesh.position.y, this.mesh.position.z );
+
+	}
+
+	public update() {
+
+		this.mesh.position.copy( this.body.position as unknown as THREE.Vector3 );
+		this.mesh.quaternion.copy( this.body.quaternion as unknown as THREE.Quaternion );
+
+		let scale = this.animator.get<number>( 'Sec4TextScale' + this.mesh.uuid ) || 0;
+
+		this.mesh.scale.set( scale, scale, scale );
+		this.boxShape.halfExtents.set( this.baseSize.x * scale, this.baseSize.y * scale, this.baseSize.z * scale );
+
+	}
+
+	public small() {
+
+		this.animator.animate( 'Sec4TextScale' + this.mesh.uuid, 0, 10 );
+
+	}
+
+}
--- src/ts/MainScene/World/Sections/Section4/index.ts
+++ src/ts/MainScene/World/Sections/Section4/index.ts
@@ -1,16 +1,27 @@
 import * as THREE from 'three';
 import * as ORE from 'ore-three';
+import * as CANNON from 'cannon';
 
 import { Section } from '../Section';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { BG } from './BG';
 import { Shadow } from './Shadow';
 import { Peoples } from './Peoples';
+import { Text } from './Text';
+
+type PhysicsObj = {
+	visual: THREE.Object3D;
+	body: CANNON.Body
+}
 
 export class Section4 extends Section {
 
 	private renderer: THREE.WebGLRenderer;
 	private peoples?: Peoples;
+
+	private cannonWorld: CANNON.World;
+	private ground?: CANNON.Body;
+	private textList: Text[] = [];
 
 	constructor( manager: THREE.LoadingManager, parentUniforms: ORE.Uniforms, renderer: THREE.WebGLRenderer ) {
 
@@ -20,6 +31,13 @@ export class Section4 extends Section {
 
 		this.bakuMaterialType = 'line';
 
+		/*-------------------------------
+			Cannon
+		-------------------------------*/
+
+		this.cannonWorld = new CANNON.World();
+		this.cannonWorld.gravity = new CANNON.Vec3( 0.0, - 9.8, 0.0 );
+
 	}
 
 	protected onLoadedGLTF( gltf: GLTF ): void {
@@ -27,6 +45,14 @@ export class Section4 extends Section {
 		let scene = gltf.scene;
 
 		this.add( scene );
+
+		/*-------------------------------
+			Light
+		-------------------------------*/
+
+		let light = new THREE.DirectionalLight();
+		light.position.set( 1, 1, 1 );
+		this.add( light );
 
 		/*-------------------------------
 			BG
@@ -48,9 +74,58 @@ export class Section4 extends Section {
 		this.peoples.position.y = - 2;
 		this.add( this.peoples );
 
+		/*-------------------------------
+			Text
+		-------------------------------*/
+
+		let textRoot = scene.getObjectByName( 'FallTexts' );
+
+		if ( textRoot ) {
+
+			textRoot.children.forEach( item => {
+
+				let text = new Text( item, this.commonUniforms );
+				this.cannonWorld.addBody( text.body );
+				this.textList.push( text );
+
+				setTimeout( () => {
+
+					text.small();
+
+				}, 2000 );
+
+			} );
+
+		}
+
+		/*-------------------------------
+			Ground
+		-------------------------------*/
+
+		let ground = scene.getObjectByName( 'Ground' );
+
+		if ( ground ) {
+
+			let size = new THREE.Box3().setFromObject( ground ).getSize( new THREE.Vector3() );
+
+			let body = new CANNON.Body( { type: CANNON.Body.KINEMATIC } );
+			body.addShape( new CANNON.Box( new CANNON.Vec3( size.x / 2, size.y / 2, size.z / 2 ) ) );
+			body.position.set( ground.position.x, ground.position.y, ground.position.z );
+			this.cannonWorld.addBody( body );
+
+		}
+
 	}
 
 	public update( deltaTime: number ): void {
+
+		this.cannonWorld.step( deltaTime );
+
+		this.textList.forEach( item => {
+
+			item.update();
+
+		} );
 
 		if ( this.peoples ) {
 
