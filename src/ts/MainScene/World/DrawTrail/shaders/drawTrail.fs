11ee1aa0b92ae23a54368ba73ef92ea2f1547bea
ukonpower <ukon.ryohei@junni.jp>
2022-09-27 22:19:00 +0900

    trail pbrやっぱいる

--- src/ts/MainScene/World/DrawTrail/shaders/drawTrail.fs
+++ src/ts/MainScene/World/DrawTrail/shaders/drawTrail.fs
@@ -271,6 +271,83 @@ varying vec3 vWorldPos;
 
 #endif
 
+
+float ggx( float dNH, float roughness ) {
+	
+	float a2 = roughness * roughness;
+	a2 = a2 * a2;
+	float dNH2 = dNH * dNH;
+
+	if( dNH2 <= 0.0 ) return 0.0;
+
+	return a2 / ( PI * pow( dNH2 * ( a2 - 1.0 ) + 1.0, 2.0) );
+
+}
+
+vec3 lambert( vec3 diffuseColor ) {
+
+	return diffuseColor / PI;
+
+}
+
+float gSchlick( float d, float k ) {
+
+	if( d == 0.0 ) return 0.0;
+
+	return d / ( d * ( 1.0 - k ) + k );
+	
+}
+
+float gSmith( float dNV, float dNL, float roughness ) {
+
+	float k = clamp( roughness * sqrt( 2.0 / PI ), 0.0, 1.0 );
+
+	return gSchlick( dNV, k ) * gSchlick( dNL, k );
+	
+}
+
+float fresnel( float d ) {
+	
+	float f0 = 0.15;
+
+	return f0 + ( 1.0 - f0 ) * pow( 1.0 - d, 5.0 );
+
+}
+
+
+vec3 RE( Geometry geo, Material mat, Light light) {
+
+	vec3 lightDir = normalize( light.direction );
+	vec3 halfVec = normalize( geo.viewDir + lightDir );
+
+	float dLH = clamp( dot( lightDir, halfVec ), 0.0, 1.0 );
+	float dNH = clamp( dot( geo.normal, halfVec ), 0.0, 1.0 );
+	float dNV = clamp( dot( geo.normal, geo.viewDir ), 0.0, 1.0 );
+	float dNL = clamp( dot( geo.normal, lightDir), 0.0, 1.0 );
+
+	vec3 irradiance = light.color * dNL;
+
+	// diffuse
+	vec3 diffuse = lambert( mat.diffuseColor ) * irradiance;
+
+	// specular
+	float D = ggx( dNH, mat.roughness );
+	float G = gSmith( dNV, dNL, mat.roughness );
+	float F = fresnel( dLH );
+	
+	vec3 specular = (( D * G * F ) / ( 4.0 * dNL * dNV + 0.0001 ) * mat.specularColor ) * irradiance; 
+
+	vec3 c = vec3( 0.0 );
+	c += diffuse * ( 1.0 - F ) + specular;
+
+	
+	vec3 color = vec3( mix( vec3( 1.0 ), mat.diffuseColor * 0.5 + 0.5, length( mat.diffuseColor ) ) )* mix( #000, vec3( 1.0, 1.0, 1.0 ), dNL + random(gl_FragCoord.xy * 0.001) * 0.15 );
+	// c = mix( c, vec3( 1.0 ), uLine);
+
+	return c;
+
+}
+
 /*-------------------------------
 	HSV
 -------------------------------*/
@@ -296,10 +373,14 @@ void main( void ) {
 	mat.roughness = 0.1;
 	mat.metalness = 0.0;
 
-	// emission
-
 	vec3 gradation = hsv2rgb( vec3( time * 0.1 + vUv.y * 0.1 + 0.1, 1.0 - uMaterial[2] * 0.5, 1.0 ) ) * (uMaterial[0] + uMaterial[2] + uMaterial[3]) * 0.9;
 
+	// albedo
+
+	mat.albedo += gradation * uMaterial[0];
+
+	// emission
+	
 	mat.emission = vec3( 0.0 );
 	mat.emission += gradation * uMaterial[0];
 	mat.emission += gradation * uMaterial[2];
@@ -307,6 +388,10 @@ void main( void ) {
 	mat.emission += vec3( 0.1, 0.0, 0.0 ) * uMaterial[3];
 	mat.emission += vec3( 0.5 ) * uMaterial[4];
 	mat.emission += vec3( 1.0 ) * uMaterial[5];
+
+	
+	mat.diffuseColor = mix( mat.albedo, vec3( 0.0, 0.0, 0.0 ), mat.metalness );
+	mat.specularColor = mix( vec3( 1.0, 1.0, 1.0 ), mat.albedo, mat.metalness );
 
 	// output
 
@@ -374,6 +459,30 @@ void main( void ) {
 	outColor += (refractCol) * hsv2rgb(vec3( time * 0.05, 1.0, 1.0 ) ) * uMaterial[1];
 
 	/*-------------------------------
+		PBR
+	-------------------------------*/
+
+	Light light;
+
+	float lw;
+	lw += uMaterial[0];
+
+	#if NUM_DIR_LIGHTS > 0
+
+		#pragma unroll_loop_start
+			for ( int i = 0; i < NUM_DIR_LIGHTS; i ++ ) {
+
+				light.direction = directionalLights[ i ].direction;
+				light.color = directionalLights[ i ].color;
+
+				outColor += RE( geo, mat, light ) * lw;
+				
+			}
+		#pragma unroll_loop_end
+
+	#endif
+
+	/*-------------------------------
 		Emission
 	-------------------------------*/
 
@@ -382,10 +491,6 @@ void main( void ) {
 	/*-------------------------------
 		Shadow
 	-------------------------------*/
-
-	Light light;
-
-	float lw;
 
 	#if NUM_DIR_LIGHTS > 0
 
